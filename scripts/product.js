50a4e57dbe8c905ebe607ce862807a32b2dc3168
Zhongjie Shen <zhongjis@uci.edu>
2019-04-22 22:31:43 -0700

    add findOutMore buttons to detailed product pages

--- scripts/product.js
+++ scripts/product.js
@@ -1,3 +1,8 @@
 function getMoreDetail(obj) {
 	window.location.href = obj+'.html';
 }
+
+function findOutMore(obj) {
+	var link = 'https://www.google.com/search?tbm=isch&q='+obj
+	window.open(link, '_blank');
+}
