72a56e4ceafd9b0e6ac075a8a7fdf022a17d62a1
Ubuntu <ubuntu@ip-172-31-83-244.ec2.internal>
2019-05-04 22:06:37 +0000

    add header.php

---
+++ header.php
@@ -0,0 +1,7 @@
+	<body>
+		<a id="website-link" href="index.php">wedoeverythingforyou.com</a>
+		<div id="navbar">
+			<a href="index.php">About Us</a>
+			<a href="product.php">Products</a>
+		</div>
+	</body>
