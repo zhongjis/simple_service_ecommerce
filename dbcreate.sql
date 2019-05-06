05d2c44a3f1aab5ed800b36a38efe30b96bda35d
Ubuntu <ubuntu@ip-172-31-83-244.ec2.internal>
2019-05-06 23:17:42 +0000

    update dbcreate file

--- dbcreate.sql
+++ dbcreate.sql
@@ -5,6 +5,33 @@ CREATE TABLE products (
     brief_description text,
     detail_description text
 );
-    
+
 INSERT INTO products
 VALUES (1, "images/babylion.jpg", "Baby Lion Service", "Want a baby lion at home? Craving the life with lion King? We can help you!", "Want a baby lion at home? Craving the life with lion King? We can help you! Just fill out the form below and we can help you achieve your dream!");
+
+INSERT INTO products
+VALUES (2, "images/camel.jpeg", "Camel Service", "Want a camel at home and ride it anywhere you want? We can help you!", "Want a camel at home and ride it anywhere you want? We can help you! Just fill out the form below and we can help you achieve your dream!");
+
+INSERT INTO products
+VALUES (3, "images/cancun.jpeg", "Cancun Settlement Service", "Want to buy an island near Cancun? Want to have your own beach? We can help you!", "Want to buy an island near Cancun? Want to have your own beach? We can help you! Just fill out the form below and we can help you achieve your dream!");
+
+INSERT INTO products
+VALUES (4, "images/chongqing.jpg", "Chongqing, China Settlement Service", "Want to live in Chongqing, China and have Hotpot everyday? We can help you!", "Want to live in Chongqing, China and have Hotpot everyday? We can help you! Just fill out the form below and we can help you achieve your dream!");
+
+INSERT INTO products
+VALUES (5, "images/effelTower.jpeg", "Effel Tower Settlement Service", "Want to have your own living space on Effel Tower? We can help you!", "Want to have your own living space on Effel Tower? We can help you! Just fill out the form below and we can help you achieve your dream!");
+
+INSERT INTO products
+VALUES (6, "images/'hollywood'.jpeg", "Hollywood Settlement Service", "Want to buy the whole Hollywood? We can help you!", "Want to buy the whole Hollywood? We can help you! Just fill out the form below and we can help you achieve your dream!");
+
+INSERT INTO products
+VALUES (7, "images/moscow.jpeg", "Moscow Settlement Service", "Want to live in Moscow? We can help you!", "Want to live in Moscow? We can help you! Just fill out the form below and we can help you achieve your dream!");
+
+INSERT INTO products
+VALUES (8, "images/panda-1.jpeg", "Baby Panda Service", "Want a Panda at home? We can help you!", "Want a Panda at home? We can help you! Just fill out the form below and we can help you achieve your dream!");
+
+INSERT INTO products
+VALUES (9, "images/somewherenice.jpg", "Valley Settlement Service", "Want to own your own valley? We can help you!", "Want to own your own valley? We can help you! Just fill out the form below and we can help you achieve your dream!");
+
+INSERT INTO products
+VALUES (10, "images/xuancheng.jpeg", "Budda at Home Service", "Want to have your own budda at home? We can help you!", "Want to have your own budda at home? We can help you! Just fill out the form below and we can help you achieve your dream!");
