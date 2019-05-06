9364cd3367a104512c920b4be53b39b1542480fe
Ubuntu <ubuntu@ip-172-31-83-244.ec2.internal>
2019-05-06 23:18:40 +0000

    update dbconfig.php for openlab vm

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,6 @@
 <?php
     define("DB_USER", "root");
-    define("DB_PASSWORD", "");
+    define("DB_PASSWORD", "inf124");
     define("DB_DATABSE", "inf124");
     define("DB_HOST", "localhost");
 ?>
