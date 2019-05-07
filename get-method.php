03c28a08b93b179da906f471381f04d6ad8b1d00
Zhongjie Shen <zhongjis@uci.edu>
2019-05-07 02:13:25 -0700

    add dynamic form to get-method.php

--- get-method.php
+++ get-method.php
@@ -19,8 +19,14 @@
 		$creditNo = $_GET['creditNoField'];
 		$creditHolder = $_GET['cardHolderField'];
 
-		$query = "INSERT INTO orderinfo (product, quantity, firstname, lastname, phonenumber, address, zipcode, city, state, shippingmethod, creditcardnumber, nameofthecard) VALUES ('".
+		date_default_timezone_set('America/Los_Angeles');
+		$today = date("Ymd");
+		$rand = strtoupper(substr(uniqid(sha1(time())),0,4));
+		$order_id = $today . $rand;
+
+		$query = "INSERT INTO orderinfo (product, id, quantity, firstname, lastname, phonenumber, address, zipcode, city, state, shippingmethod, creditcardnumber, nameofthecard) VALUES ('".
 		$product_identifier."', '".
+		$order_id."', '".
 		$quantity."', '".
 		$firstname."', '".
 		$lastname."', '".
@@ -34,13 +40,36 @@
 		$creditHolder."')";
 
 		// display the results 
+		echo('<h2>Your Order</h2>');
+		
+		$stmt = $conn->prepare('SELECT * FROM orderinfo WHERE id = :id');
+		$stmt->bindParam(':id', $order_id);
+		$stmt->execute();
+
+		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+			echo('<p>Your order No.: '.$row['id'].'</p>');
+			echo('<p>Your product: '.$row['product'].'</p>');
+			echo('<p>Quantity: '.$row['quantity'].'</p>');
+			echo('<p>Your first name: '.$row['firstname'].'</p>');
+			echo('<p>Your last name: '.$row['lastname'].'</p>');
+			echo('<p>Your contact phone number: '.$row['phonenumber'].'</p>');
+			echo('<p>Your address: '.$row['address'].'</p>');
+			echo('<p>Your zip code: '.$row['zipcode'].'</p>');
+			echo('<p>Your city: '.$row['city'].'</p>');
+			echo('<p>Your state: '.$row['state'].'</p>');
+			echo('<p>Your shipping method: '.$row['shippingmethod'].'</p>');
+			echo('<p>Your credit card No.: '.$row['creditcardnumber'].'</p>');
+			echo('<p>Your card holder name: '.$row['nameofthecard'].'</p>');
+		}
+
+
 		echo('<h3>Form Submission status</h3>'); 
 		if ($conn->query($query) == TRUE) {
-			echo "success";
+			echo "success <br>";
 		} else {
 			echo "error: ". $query."<br>".$conn->error;
 		}
-		
+
 		exit;
 	// }
 ?>
