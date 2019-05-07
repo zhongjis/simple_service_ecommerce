67ccc0a20eb5d41c262a846582573705f77cebec
Zhongjie Shen <zhongjis@uci.edu>
2019-05-06 22:07:45 -0700

    now product-detail.php works. Don't worry about how we did it damn it.

--- product-detail.php
+++ product-detail.php
@@ -1,17 +1,22 @@
+<?php
+	// this php block is for db connection and data retrival 
+	require_once('dbconnect.php');
+	$conn = connect(); 
+	$query = "SELECT * FROM products";
+	$result = $conn->query($query);
+?>
+
 <!doctype html>
 <html>
 	<head>
 		<title>Product Info</title>
 		<meta charset="utf-8">
-		<link rel="stylesheet" href="styles/general.css" type="text/css">
+		<link rel="stylesheet" href="styles/product-detail.css" type="text/css">
 		<script type = "text/JavaScript" src ="ajax.js"> </script>
+		<script src="scripts/product.js"></script>
 	</head>
+	<?php require_once "header.php"; ?>
 	<body>
-		<a id="website-link" href="index.php">wedoeverythingforyou.com</a>
-		<div id="navbar">
-			<a href="index.php">About Us</a>
-			<a href="product.php">Products</a>
-		</div>
 		<?php
 			$id = $_GET["id"];
 			$query = "SELECT * FROM products WHERE id = ".$id;
@@ -22,34 +27,49 @@
 				echo('<p class="product-description">'.$row['detail_description'].'</p>');
 			}
 		?>
-		<form>
+		<form action="mailto:zhongjis@uci.edu" name="QuoteForm" method="post" enctype="text/plain" onSubmit="return (checkQuoteRequest())"> <br>
 			<h3>Get a Quote</h3>
-			Product Identifier:<br>
-			<input type="text" name="identifier"><br>
-			Quantity:<br>
-			<input type="text" name="quantity"><br>
-			First name:<br>
-			<input type="text" name="firstname"><br>
-			Last name:<br>
-			<input type="text" name="lastname"><br>
-			Phone number:<br>
-			<input type="text" name="phone"><br>
-			Shipping Address:<br>
-			<input type="text" name="address"><br>
-			Shipping Method:<br>
-			<select name="How long you want this done" id="shipping">
-				<option value="overnight">Overnight</option>
-				<option value="2-day">2-day</option>
-				<option value="6-day">6-day</option>
-			</select>
-			<div>
-				<h3>Deposit Payment Method</h3>
-				Credit Card No.:<br>
-				<input type="text" name="credit-no"><br>
-				Name on the Card:<br>
-				<input type="text" name="card-name"><br>
+			<div id="basicInfo">
+				<h4>Basic Information</h4>
+				Product Identifier: <br>
+				<select name="How long you want this done" id="shippingMethod">
+					<option value="lion">Baby Lion Service</option>
+					<option value="camel">Camel Service</option>
+					<option value="cancun">Cancun Settlement Service</option>
+					<option value="chongqing">Chongqing, China Settlement Service</option>
+					<option value="effelTower">Effel Tower Settlement Service</option>
+					<option value="hollywood">Hollywood Settlement Service</option>
+					<option value="lion">Moscow Settlement Service</option>
+					<option value="camel">Baby Panda Service</option>
+					<option value="cancun">Valley Settlement Service</option>
+					<option value="chongqing">Budda at Home Service</option>
+				</select> <br>
+				Quantity: <br> 
+				<input type="text" name="quantityField" id="quantity"> <br>
+				First Name: <br>
+				<input type="text" name="firstNameField" id="firstName"> <br>
+				Last Name: <br>
+				<input type="text" name="lastNameField" id="lastName"> <br>
+				Phone Number: <br>
+				<input type="text" name="phoneNumberField" id="phoneNumber"> <br>
+				Shipping Address: <br>
+				<input type="text" name="shippingAddressField" id="shippingAddress"> <br>
+				Shipping Method: <br>
+				<select name="shippingMethod">
+					<option value="overnight">Overnight</option>
+					<option value="2-day">2-day</option>
+					<option value="6-day">6-day</option>
+				</select> <br> <br>
 			</div>
-			<input type="submit" name="submit">
+			<div id="paymentInfo">
+				<h4>Deposit Payment Method</h4>
+				Credit Card No.:<br>
+				<input type="text" name="creditNoField" id="creditNo"><br>
+				Name on the Card:<br>
+				<input type="text" name="cardHolderField" id="creditHolder"><br>
+			</div> <br>
+			<input type="reset" value="Reset">
+			<input type="submit" value="Send">
 		</form>
 	</body>
 </html>
