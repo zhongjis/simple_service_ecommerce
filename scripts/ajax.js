c2b5ad699027c62fda17bb0f29c935290c460f33
amocz <iamchristopherzhao@gmail.com>
2019-05-06 23:54:29 -0700

    updated ajax, autocomplete state, city

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,4 +1,4 @@
-function getPlace(zipcode)
+function getPlace(zip)
 {
     var xhr = new XMLHttpRequest(); 
     xhr.onreadystatechange = function ()
@@ -9,9 +9,9 @@ function getPlace(zipcode)
             var place = result.split (', ');
             console.log(place[0], place[1]);
             if (document.getElementById ("city").value == "")
-                document.getElementById ("city").value = place[0];
+                document.getElementById ("city").value = place[1];
             if (document.getElementById ("state").value == "")
-                document.getElementById ("state").value = place[1]; 
+                document.getElementById ("state").value = place[0]; 
         }
     }
     xhr.open("GET", "getCityState.php?zip=" + zip, true);
@@ -19,39 +19,36 @@ function getPlace(zipcode)
 }
 
 
-function getPrice() {
-	var zip = document.getElementById("zipPostalCode").value;
-	var id = document.getElementById("productIdentifier").value;
-	if (window.XMLHttpRequest) {
-		var xhr = new XMLHttpRequest();
-    } else {
-		var xhr = new ActiveXObject ("Microsoft.XMLHTTP");
-    }
+// function getPrice() {
+// 	var zip = document.getElementById("zipPostalCode").value;
+// 	var id = document.getElementById("productIdentifier").value;
+//     var xhr = new XMLHttpRequest(); 
 
-	xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            var rate = parseFloat(xhr.responseText);
-			if (window.XMLHttpRequest) {
-       			var xhr2 = new XMLHttpRequest();
-    		} else {
-        		var xhr2 = new ActiveXObject ("Microsoft.XMLHTTP");
-    		}
 
-			xhr2.onreadystatechange = function () {
-        		if (xhr2.readyState == 4 && xhr2.status == 200) {
-            		var price = parseFloat(xhr2.responseText);
-					var quantity = parseFloat(document.getElementById("quantity").value);
-					document.getElementById ("total").innerHTML = "$" + (price*quantity).toFixed(2);
-            		if (zip) {
-						document.getElementById ("taxtotal").innerHTML = "$"+((price*quantity)*(1.0+rate)).toFixed(2);
-					}
-				}
-			}
-			xhr2.open("GET", "getItemPrice.php?id=" + id, true);
-    		xhr2.send();	
-        }
-    }
+// 	xhr.onreadystatechange = function () {
+//         if (xhr.readyState == 4 && xhr.status == 200) {
+//             var rate = parseFloat(xhr.responseText);
+// 			if (window.XMLHttpRequest) {
+//        			var xhr2 = new XMLHttpRequest();
+//     		} else {
+//         		var xhr2 = new ActiveXObject ("Microsoft.XMLHTTP");
+//     		}
 
-    xhr.open("GET", "getTaxRate.php?zip=" + zip, true);
-    xhr.send();
-}
+// 			xhr2.onreadystatechange = function () {
+//         		if (xhr2.readyState == 4 && xhr2.status == 200) {
+//             		var price = parseFloat(xhr2.responseText);
+// 					var quantity = parseFloat(document.getElementById("quantity").value);
+// 					document.getElementById ("total").innerHTML = "$" + (price*quantity).toFixed(2);
+//             		if (zip) {
+// 						document.getElementById ("taxtotal").innerHTML = "$"+((price*quantity)*(1.0+rate)).toFixed(2);
+// 					}
+// 				}
+// 			}
+// 			xhr2.open("GET", "getItemPrice.php?id=" + id, true);
+//     		xhr2.send();	
+//         }
+//     }
+
+//     xhr.open("GET", "getTaxRate.php?zip=" + zip, true);
+//     xhr.send();
+// }
