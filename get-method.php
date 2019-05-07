626a81953f00f07f7c5fa6900fd3fccd3fb7f558
Zhongjie Shen <zhongjis@uci.edu>
2019-05-07 01:04:22 -0700

    update get-method.php

--- get-method.php
+++ get-method.php
@@ -1,8 +1,11 @@
  <?php 
 // Check if the form is submitted 
-
-	if ( isset( $_GET['submit'] ) ) { 
+	// if ( isset( $_GET['submit'] ) ) { 
+	// 	echo "hahahahah";
 		// retrieve the form data by using the element's name attributes value as key 
+		require_once('dbconnect.php');
+		$conn = connect(); 
+		
 		$product_identifier = $_GET['productIdentifierField'];
 		$quantity = $_GET['quantityField'];
 		$firstname = $_GET['firstNameField']; 
@@ -16,9 +19,29 @@
 		$creditNo = $_GET['creditNoField'];
 		$creditHolder = $_GET['cardHolderField'];
 
+		$query = "INSERT INTO orderinfo (product, quantity, firstname, lastname, phonenumber, address, zipcode, city, state, shippingmethod, creditcardnumber, nameofthecard) VALUES ('".
+		$product_identifier."', '".
+		$quantity."', '".
+		$firstname."', '".
+		$lastname."', '".
+		$phoneNumber."', '".
+		$shippingAddress."', '".
+		$zipCode."', '".
+		$city."', '".
+		$state."', '".
+		$shippingMethod."', '".
+		$creditNo."', '".
+		$creditHolder."')";
+
 		// display the results 
-		echo '<h3>Form GET Method</h3>'; 
-		echo 'Your name is ' . $lastname . ' ' . $firstname; exit;
-	}
+		echo('<h3>Form Submission status</h3>'); 
+		if ($conn->query($query) == TRUE) {
+			echo "success";
+		} else {
+			echo "error: ". $query."<br>".$conn->error;
+		}
+		
+		exit;
+	// }
 ?>
  
