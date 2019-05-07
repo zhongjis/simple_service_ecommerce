b60261a824244af86059dfbdd46671d8d1a64c16
Zhongjie Shen <zhongjis@uci.edu>
2019-05-07 03:32:15 -0700

    first state for get-method.php

--- get-method.php
+++ get-method.php
@@ -43,32 +43,47 @@
 		echo('<h2>Your Order</h2>');
 		
 		$stmt = $conn->prepare('SELECT * FROM orderinfo WHERE id = :id');
-		$stmt->bindParam(':id', $order_id);
-		$stmt->execute();
 
-		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-			echo('<p>Your order No.: '.$row['id'].'</p>');
-			echo('<p>Your product: '.$row['product'].'</p>');
-			echo('<p>Quantity: '.$row['quantity'].'</p>');
-			echo('<p>Your first name: '.$row['firstname'].'</p>');
-			echo('<p>Your last name: '.$row['lastname'].'</p>');
-			echo('<p>Your contact phone number: '.$row['phonenumber'].'</p>');
-			echo('<p>Your address: '.$row['address'].'</p>');
-			echo('<p>Your zip code: '.$row['zipcode'].'</p>');
-			echo('<p>Your city: '.$row['city'].'</p>');
-			echo('<p>Your state: '.$row['state'].'</p>');
-			echo('<p>Your shipping method: '.$row['shippingmethod'].'</p>');
-			echo('<p>Your credit card No.: '.$row['creditcardnumber'].'</p>');
-			echo('<p>Your card holder name: '.$row['nameofthecard'].'</p>');
+		function getRows() {
+			global $conn, $order_id, $stmt;
+			$stmt->bindParam(':id', $order_id);
+			$stmt->execute();
 		}
 
-
-		echo('<h3>Form Submission status</h3>'); 
-		if ($conn->query($query) == TRUE) {
-			echo "success <br>";
-		} else {
-			echo "error: ". $query."<br>".$conn->error;
+		function showConfirmation(){
+			global $stmt;
+			while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+				echo('<p>Your order No.: '.$row['id'].'</p>');
+				echo('<p>Your product: '.$row['product'].'</p>');
+				echo('<p>Quantity: '.$row['quantity'].'</p>');
+				echo('<p>Your first name: '.$row['firstname'].'</p>');
+				echo('<p>Your last name: '.$row['lastname'].'</p>');
+				echo('<p>Your contact phone number: '.$row['phonenumber'].'</p>');
+				echo('<p>Your address: '.$row['address'].'</p>');
+				echo('<p>Your zip code: '.$row['zipcode'].'</p>');
+				echo('<p>Your city: '.$row['city'].'</p>');
+				echo('<p>Your state: '.$row['state'].'</p>');
+				echo('<p>Your shipping method: '.$row['shippingmethod'].'</p>');
+				echo('<p>Your credit card No.: '.$row['creditcardnumber'].'</p>');
+				echo('<p>Your card holder name: '.$row['nameofthecard'].'</p>');
+			}		
 		}
+
+		function showSubmission() {
+			global $conn, $query;
+			echo('<h3>Form Submission status</h3>'); 
+			if ($conn->query($query) == TRUE) {
+				echo "success <br>";
+			} else {
+				echo "error: ". $query."<br>".$conn->error;
+			}
+		}
+
+		getRows();
+		showConfirmation();
+		showSubmission();
+
+		header("confirmation.php?id=".$order_id);
 
 		exit;
 	// }
