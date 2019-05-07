732551c7fa17c939e437379bf6712321ad3064c8
Zhongjie Shen <zhongjis@uci.edu>
2019-05-06 22:08:59 -0700

    link header css file to header.php

--- header.php
+++ header.php
@@ -1,7 +1,10 @@
-	<body>
-		<a id="website-link" href="index.php">wedoeverythingforyou.com</a>
-		<div id="navbar">
-			<a href="index.php">About Us</a>
-			<a href="product.php">Products</a>
-		</div>
-	</body>
+<head>
+	<link rel="stylesheet" href="styles/general.css" type="text/css">
+</head>
+<body>
+	<a id="website-link" href="index.php">wedoeverythingforyou.com</a>
+	<div id="navbar">
+		<a href="index.php">About Us</a>
+		<a href="product.php">Products</a>
+	</div>
+</body>
